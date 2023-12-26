2064c4d61eab3c7f2c07f0b1207a4940f20024e0
Royal VICTOR <royal.victor@gmail.com>
2023-12-26 09:23:36 +0100

    AuthStore Debug

--- stores/AuthStore.ts
+++ stores/AuthStore.ts
@@ -7,21 +7,26 @@ export const useAuthStore = defineStore('auth', () => {
   const isLoggedIn = ref(false)
 
   const login = async (email: string, password: string) => {
-    const fetchResponse = useFetch($api('/login')).post({
-      email,
-      password,
-    })
-    const { isFetching, error, data } = await fetchResponse
-
-    if (data.value) {
-      const response = JSON.parse(data.value).loginResponse
-      if (response.token) {
-        isLoggedIn.value = true
-
-        document.cookie = `access_token=${response.token};secure; samesite=none;`
-        console.log("le cookie vient d'être créé")
-        return true
+    console.log('Entrée dans le login côté store')
+    try {
+      console.log("$api('/login') :>> ", $api('/login'))
+      const fetchResponse = useFetch($api('/login')).post({
+        email,
+        password,
+      })
+      const { isFetching, error, data } = await fetchResponse
+      if (data.value) {
+        const response = JSON.parse(data.value).loginResponse
+        if (response.token) {
+          isLoggedIn.value = true
+          console.log('response.token :>> ', response.token)
+          document.cookie = `access_token=${response.token};secure; samesite=none;`
+          console.log("le cookie vient d'être créé")
+          return true
+        }
       }
+    } catch (error) {
+      console.log('error :>> ', error)
     }
     // TODO: gérer l'erreur (avec un store dédié aux erreurs ?)
     return false
