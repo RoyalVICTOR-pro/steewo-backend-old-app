6159194e077b52ab01106fd90c4a43310ed6872a
Royal VICTOR <rvictor@cbi-multimedia.com>
2024-01-19 16:37:18 +0100

    Bug Fix on Validator Schema

--- composables/ValidatorSelector.ts
+++ composables/ValidatorSelector.ts
@@ -2,10 +2,10 @@ import { z } from 'zod'
 
 export const useValidatorSelector = () => {
   const { t } = useI18n()
-  const professionSchema = z.object({
+  const professionSchema = {
     name: z.string({ required_error: t('bo.forms.errors.required') }),
     is_enabled: z.boolean({ required_error: t('bo.forms.errors.required') }),
-  })
+  }
 
   return {
     professionSchema,
