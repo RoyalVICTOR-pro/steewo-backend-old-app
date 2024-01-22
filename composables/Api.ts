1bc68a9a9b25765e5334305df9dee41ea6158b2a
Royal VICTOR <rvictor@cbi-multimedia.com>
2024-01-22 16:31:10 +0100

    Replacing useFetch with $fetch in Api Composable

--- composables/Api.ts
+++ composables/Api.ts
@@ -16,15 +16,6 @@ export const useApi = () => {
   }
 
   const get = async (endpoint) => {
-    /* 
-    const { data, error } = await useFetch($api(endpoint), {
-      credentials: 'include',
-    })
-    if (error.value) {
-      handleApiErrors(error.value)
-    }
-    return data.value
-    */
     try {
       const data = await $fetch($api(endpoint), {
         credentials: 'include',
@@ -36,16 +27,6 @@ export const useApi = () => {
   }
 
   const post = async (endpoint, content = {}) => {
-    /*     const { data, error } = await useFetch($api(endpoint), {
-      method: 'POST',
-      credentials: 'include',
-      body: content,
-    })
-    if (error.value) {
-      handleApiErrors(error.value)
-    } 
-    return data.value
-    */
     try {
       const data = await $fetch($api(endpoint), {
         method: 'POST',
@@ -54,12 +35,9 @@ export const useApi = () => {
       })
       return data
     } catch (error) {
-      console.log(' Passage 1')
       return handleApiErrors(error)
     }
   }
-
-  // Vous pouvez ajouter ici les méthodes put, patch, delete
 
   return { get, post }
 }
