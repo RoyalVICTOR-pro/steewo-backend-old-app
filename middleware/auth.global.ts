54baa83c291c23571aa2cc147287a56a50bd19fc
Royal VICTOR <rvictor@cbi-multimedia.com>
2024-02-26 16:01:29 +0100

    Bug Fix : Redirect to login/ show the login form in the main page

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -2,24 +2,24 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   const authStore = useAuthStore()
   const { isLoggedIn } = storeToRefs(authStore)
 
-  if (to.path === '/login') {
+  if (to.path.includes('login')) {
     setPageLayout('blank')
   } else {
     setPageLayout('default')
   }
 
-  const routeToGo = to.path === '/login' ? '/' : to.path
+  const routeToGo = to.path.includes('login') ? '/' : to.path
 
   await authStore.checkIfLoggedIn(routeToGo)
 
-  if (!isLoggedIn.value && to.path !== '/login') {
+  if (!isLoggedIn.value && !to.path.includes('login')) {
     return navigateTo({
       path: '/login',
       query: { redirect: routeToGo.replace('/', '') },
     })
   }
 
-  if (isLoggedIn.value && to.path === '/login') {
+  if (isLoggedIn.value && to.path.includes('login')) {
     return navigateTo('/')
   }
 })
